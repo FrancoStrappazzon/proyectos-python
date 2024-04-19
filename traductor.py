3248c22d9207d7de8d5dfdcdfd784f246a31d0ba
Franco St <franco.strappazzon@mi.unc.edu.ar>
2024-04-19 16:52:34 -0300

    Agregando excepciones al traductor

--- traductor.py
+++ traductor.py
@@ -1,9 +1,9 @@
 from translate import Translator
 
-translator = Translator(from_lang='spanish', to_lang='english')
-
-texto = input("Que quieres traducir? :")
-
-respuesta = translator.translate(texto)
-
-print(respuesta)
+try:
+    translator = Translator(from_lang='spanish', to_lang='english')
+    texto = input("¿Qué quieres traducir?: ")
+    respuesta = translator.translate(texto)
+    print("Texto traducido: ", respuesta)
+except Exception as e:
+    print("Ocurrió un error durante la traducción:", e)
