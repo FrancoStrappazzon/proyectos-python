c96fd4d508b4b7b2d7de69bb559d6b013f9920e9
Franco St <franco.strappazzon@mi.unc.edu.ar>
2024-04-23 15:39:10 -0300

    Agrego seleccion de diferentes idiomas para la traduccion

--- traductor.py
+++ traductor.py
@@ -1,8 +1,28 @@
 from deep_translator import GoogleTranslator
 
-try:
-    texto = input("¿Qué quieres traducir?: ")
-    respuesta = GoogleTranslator(source="spanish", target = "english").translate(texto)
-    print("Texto traducido: ", respuesta)
-except Exception as e:
-    print("Ocurrió un error durante la traducción:", e)
+idiomas_permitidos = ["ingles", "portugues", "italiano"]
+
+while True:
+    seleccion = input("Seleccione el idioma en el cual quiere su traducción: Inglés, Portugués o Italiano: \n").lower()
+    
+    if seleccion in idiomas_permitidos:
+    
+        texto = input("¿Qué quieres traducir?: \n")
+    
+        try:
+            if seleccion == "ingles":
+                respuesta = GoogleTranslator(source="spanish", target="english").translate(texto)
+            elif seleccion == "portugues":
+                respuesta = GoogleTranslator(source="spanish", target="portuguese").translate(texto)
+            elif seleccion == "italiano":
+                respuesta = GoogleTranslator(source="spanish", target="italian").translate(texto)
+        
+            print("Texto traducido: ", respuesta)
+    
+        except Exception as e:
+            print("Ocurrió un error durante la traducción:", e)
+
+    elif seleccion not in idiomas_permitidos: 
+        print("ERROR, por favor seleccione uno de los idiomas disponibles")
+        continue
+
