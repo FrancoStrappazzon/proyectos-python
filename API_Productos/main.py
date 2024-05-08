9e613f618eab6fcc5fb0c13a4cc822e002e566dc
Franco St <franco.strappazzon@mi.unc.edu.ar>
2024-05-08 08:58:59 -0300

    Agrego funciones de actualizar_producto y borrar_producto

--- API_Productos/main.py
+++ API_Productos/main.py
@@ -1,3 +1,6 @@
+#Programa para crear una API de gestion de productos conectada con una base de datos en Sqlite
+#en la cual se tienen operaciones CRUD(Create, Read,Update,Delete)
+
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from database import connect_to_database, close_database_connection
@@ -95,20 +98,42 @@ def mostrar_producto(product_id,db_conn : sqlite3.Connection = Depends(get_db_co
         return {"producto":product_dict}    #Devuelvo el resultado como JSON
     
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al obtener los el producto con id: {product_id}: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error al obtener el producto con id: {product_id}: {str(e)}")
 
 #Actualizar un producto
 @app.put('/products/{product_id}')
-def actualizar_producto(product_id):
-    return "producto actualizado"
-
+def actualizar_producto(product_id: int, product: Product,db_conn : sqlite3.Connection = Depends(get_db_connection)):
+    try:
+        #Me conecto con la db
+        cursor = db_conn.cursor()
+        #actualizo el producto
+        cursor.execute("UPDATE products SET name = ?, price = ?, quantity = ? WHERE id = ?",
+                       ( product.name, product.price, product.quantity,product_id))
+         # Confirmo los cambios en la base de datos
+        db_conn.commit()
+        
+        return {"mensaje": "Producto actualizado exitosamente"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al actualizar el producto con id: {product_id}: {str(e)}")
 
 #Borrar un producto
 @app.delete('/products/{product_id}')
-def borrar_producto(product_id):
-    return "Producto borrado"
-
-
+def borrar_producto(product_id:int, product: Product,db_conn : sqlite3.Connection = Depends(get_db_connection)):
+    try:
+        #Me conecto con la db
+        cursor = db_conn.cursor()
+        #borro el producto
+        cursor.execute("DELETE FROM products WHERE id = ?",(product_id,))
+         # Confirmo los cambios en la base de datos
+        db_conn.commit()
+        
+        return {"message":"Producto borrado exitosamente"}
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al borrar el producto con id: {product_id}: {str(e)}")
+    
+    
 
 
 #PARA EJECUTAR LA API uvicorn main:app --reload
